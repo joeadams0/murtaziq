38653aee43bf7353a821dcdb9f8c820d1e04e06c
Andrew Bruss <apb61@case.edu>
2013-10-28 13:06:22 -0400

    added test/ folder with a sample unit test and added superagent, expect.js, mocha, and instanbul (all for testing and code coverage) to package.json

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var request = require('superagent');
+var expect = require('expect.js');
+var assert = require("assert")
+describe('Array', function(){
+  describe('#testOne()', function(){
+    it('this test is just asserting true', function(){
+      assert.equal(1, 1);
+      assert.equal(2, 2);
+    })
+  })
+})
