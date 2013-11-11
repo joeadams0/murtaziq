5d1f86acd03f81dca4171b1b7a468fe7c9be0b58
Joshua Schwarz <jjs209@case.edu>
2013-11-11 04:37:19 +0000

    Update to Matchmaking controller - query modifier on lookup for all users

--- api/controllers/MatchmakingController.js
+++ api/controllers/MatchmakingController.js
@@ -13,8 +13,8 @@ module.exports = {
     index : function(req, res){
 		userId = UserHelper.getSession(req).id;
 		
-		//TODO: update this to exclude current user from allUsers and only include users in matchmaking queue
-		UserHelper.getUsers({}, function(err, allUsers) {
+		//TODO: update this to only include users in matchmaking queue
+		UserHelper.getUsers({ id: { '!': userId }}, function(err, allUsers) {
 			User.findOne({id: userId}).done(function(err, user) {
 				res.view('play/matchmaking', {currentUser : user.username, users : allUsers});
 			})
