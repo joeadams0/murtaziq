9a939f10fabd381127aa790b6e68d638e966b32b
Joshua Schwarz <jjs209@case.edu>
2013-11-13 05:15:08 +0000

    Started friendship controller and add friend button on /userlist.

---
+++ api/controllers/FriendshipController.js
@@ -0,0 +1,20 @@
+/**
+ * FriendshipController
+ *
+ * @module      :: Controller
+ * @description	:: Controller for adding and approving friendship.
+ */
+
+var _ = require('underscore');
+module.exports = {
+    
+  
+   add: function (req, res) {
+    
+    // Send a JSON response
+    return res.json({
+      hello: 'world'
+    });
+  },
+
+};
